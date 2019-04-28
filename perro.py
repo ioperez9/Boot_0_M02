b8f35deff8e4414f8f2fbf20a06b255715a1ccd6
Inma <Inma@Inma.local>
2019-04-28 10:48:18 +0200

    __str__ en perro

--- perro.py
+++ perro.py
@@ -3,3 +3,12 @@ class Perro():
         self.nombre = nombre
         self.edad = edad
         self.peso = peso
+    
+    def ladrar(self):
+        if self.peso >= 8:
+            print('GUAU, GUAU')
+        else:
+            print('Guau, guau')
+    
+    def __str__(self):
+        return "Perro: {}, edad: {}, peso: {}".format(self.nombre, self.edad, self.peso)
