12b65bcafdbc5cfa17c8399257d2dc619d1b7ded
Inma <Inma@Inma.local>
2019-04-28 11:33:10 +0200

    Herencia y subclases, perro asistencia

--- perro.py
+++ perro.py
@@ -12,3 +12,22 @@ class Perro():
     
     def __str__(self):
         return "Perro: {}, edad: {}, peso: {}".format(self.nombre, self.edad, self.peso)
+    
+class PerroAsistencia(Perro):
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo
+        self.trabajando = False
+    
+    def __str__(self):
+        return "Perro de asistencia de {}".format(self.amo)
+    
+    def pasear(self):
+        print("{} ayudo a mi dueño {} a pasear".format(self.nombre, self.amo))
+        
+    def ladrar(self):
+        if self.trabajando:
+            print("shhhh, no puedo ladrar")
+        else:
+            Perro.ladrar(self)
+        
