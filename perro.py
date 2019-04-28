02c0c1a22b381457b8905f16962259f919637bf9
Inma <Inma@Inma.local>
2019-04-28 12:06:59 +0200

    Métodos y funciones Setter y Getter

--- perro.py
+++ perro.py
@@ -1,3 +1,18 @@
+class Dog():
+    def __init__(self):
+        self.nombre = ""
+        self.edad = None
+        self.peso = None
+    
+    def ladrar(self):
+        if self.peso == None:
+            print("soy un fantasma")
+        
+        if self.peso >= 8:
+            print('GUAU, GUAU')
+        else:
+            print('ay, ay')
+
 class Perro():
     def __init__(self, nombre, edad, peso):
         self.nombre = nombre
@@ -17,7 +32,7 @@ class PerroAsistencia(Perro):
     def __init__(self, nombre, edad, peso, amo):
         Perro.__init__(self, nombre, edad, peso)
         self.amo = amo
-        self.trabajando = False
+        self.__trabajando = False
     
     def __str__(self):
         return "Perro de asistencia de {}".format(self.amo)
@@ -26,8 +41,13 @@ class PerroAsistencia(Perro):
         print("{} ayudo a mi dueño {} a pasear".format(self.nombre, self.amo))
         
     def ladrar(self):
-        if self.trabajando:
+        if self.__trabajando:
             print("shhhh, no puedo ladrar")
         else:
             Perro.ladrar(self)
         
+    def trabajando(self, valor=None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
